58879d12a78a4b2794ba9fe66d0a9294d8ea24c4
Chalermchai Wauwai <phetangelb@gmail.com>
2025-05-29 12:14:06 +0700

    commit

---
+++ app/(private)/admin/layout.tsx
@@ -0,0 +1,12 @@
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return (
+    <SessionProvider>
+      <div className="admin-wrapper">
+        {children}
+      </div>
+    </SessionProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>
-          {children}
-        </SessionProvider>
+        {children}
       </body>
     </html>
   );
