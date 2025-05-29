c2111a7274c9145114619695dabc1a1c59f70304
Chalermchai Wauwai <phetangelb@gmail.com>
2025-05-29 11:38:18 +0700

    commit

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
+
+  if (isAdminRoute && !token) {
+    const loginUrl = new URL("/admin/login", req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
