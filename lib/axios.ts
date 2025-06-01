037ff1d2329eb3021bd2a63ea0e8675cca854d0f
Chalermchai Wauwai <59055693+phetchalermchai@users.noreply.github.com>
2025-06-01 18:20:49 +0700

    commit

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getSession } from 'next-auth/react'
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_COMPLAINTS,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
